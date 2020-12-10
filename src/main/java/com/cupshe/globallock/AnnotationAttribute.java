7f41652510a677a8e4a879cf527c5512804a38cc
zxy <84235789@qq.com>
2020-12-10 09:05:27 +0800

    .

--- src/main/java/com/cupshe/globallock/AnnotationAttribute.java
+++ src/main/java/com/cupshe/globallock/AnnotationAttribute.java
@@ -29,8 +29,55 @@ public class AnnotationAttribute {
         this.policy = policy;
     }
 
-    public static AnnotationAttribute of(String key, String namespace, long leaseTime, long waitTime,
-                                         TimeUnit timeUnit, LockedPolicy policy) {
-        return new AnnotationAttribute(key, namespace, leaseTime, waitTime, timeUnit, policy);
+    public static AnnotationAttributeBuilder annotationAttributeBuilder() {
+        return new AnnotationAttributeBuilder();
+    }
+
+    /**
+     * AnnotationAttributeBuilder
+     */
+    public static class AnnotationAttributeBuilder {
+        private String _key;
+        private String _namespace;
+        private long _leaseTime;
+        private long _waitTime;
+        private TimeUnit _timeUnit;
+        private LockedPolicy _policy;
+
+        private AnnotationAttributeBuilder() {}
+
+        public AnnotationAttribute build() {
+            return new AnnotationAttribute(_key, _namespace, _leaseTime, _waitTime, _timeUnit, _policy);
+        }
+
+        public AnnotationAttributeBuilder setKey(String key) {
+            _key = key;
+            return this;
+        }
+
+        public AnnotationAttributeBuilder setNamespace(String namespace) {
+            _namespace = namespace;
+            return this;
+        }
+
+        public AnnotationAttributeBuilder setLeaseTime(long leaseTime) {
+            _leaseTime = leaseTime;
+            return this;
+        }
+
+        public AnnotationAttributeBuilder setWaitTime(long waitTime) {
+            _waitTime = waitTime;
+            return this;
+        }
+
+        public AnnotationAttributeBuilder setTimeUnit(TimeUnit timeUnit) {
+            _timeUnit = timeUnit;
+            return this;
+        }
+
+        public AnnotationAttributeBuilder setPolicy(LockedPolicy policy) {
+            _policy = policy;
+            return this;
+        }
     }
 }
